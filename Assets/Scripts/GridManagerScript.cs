d3bf48e3bee078f04da5377157c6edfab1f6b746
Luke Lin <lukelin2009@hotmail.com>
2019-04-03 11:41:41 -0400

    Initial commit

---
+++ Assets/Scripts/GridManagerScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManagerScript : MonoBehaviour
+{
+    private const int WIDTH = 6;
+    private const int HEIGHT = 10;
+
+    private GameObject[,] TileGrid;
+    public GameObject TilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TileGrid = new GameObject[WIDTH, HEIGHT];
+        MakeGrid();
+        CenterCamera();
+
+        TileScript ts = CheckMatches();
+        while(ts != null)
+        {
+            ts.SetType();
+            ts = CheckMatches();
+        }
+    }
+
+    private void OnGUI()
+    {
+        if(Input.GetMouseButton(0))
+    }
+
+    void MakeGrid()
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+            for (int y = 0; y < HEIGHT; y++)
+            {
+                GameObject tile = Instantiate(TilePrefab);
+                tile.transform.SetParent(this.transform);
+                tile.transform.localPosition = new Vector2(x, y);
+
+                TileGrid[x, y] = tile;
+            }
+        }
+    }
+
+    void CenterCamera()
+    {
+        Vector2 MiddleTilePos = TileGrid[WIDTH / 2, HEIGHT / 2].transform.position;
+        Camera.main.gameObject.transform.position = new Vector3 (MiddleTilePos.x - 0.5f, MiddleTilePos.y - 0.5f, -10);
+    }
+
+    public TileScript CheckMatches()
+    {
+        for(int x = 0; x < WIDTH; x++) {
+            for(int y = 0; y < HEIGHT; y++) {
+                TileScript ts = TileGrid[x, y].GetComponent<TileScript>();
+
+                if(x < WIDTH - 2 && ts.CompareTiles(TileGrid[x + 1, y], TileGrid[x + 2, y]))
+                {
+                    return ts;
+                }
+                else if ( y < HEIGHT -2 && ts.CompareTiles(TileGrid[x, y + 1], TileGrid[x, y + 2]))
+                {
+                    return ts;
+                }
+            }
+        }
+
+        return null;
+    }
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    public Sprite[] TileSprites;
+    public int TileType;
+
+    SpriteRenderer sr;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        SetType();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetType()
+    {
+        TileType = Random.Range(0, TileSprites.Length);
+        sr.sprite = TileSprites[TileType];
+    }
+
+    public bool CompareTiles(GameObject t1, GameObject t2)
+    {
+        TileScript ts1 = t1.GetComponent<TileScript>();
+        TileScript ts2 = t2.GetComponent<TileScript>();
+
+        return (ts1.TileType == TileType && ts2.TileType == TileType);
+    }
+}
